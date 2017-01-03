890a38c23bdd3469025da5d032e87120e924d52b
TheNew000 <mahkatorocks@gmail.com>
2017-01-03 17:54:22 -0500

    ADP Code Challenge
    A quick recursive algorithm completed in an hour

---
+++ onlyEven.js
@@ -0,0 +1,27 @@
+// Problem: Using Javascript, given an array of n integers (example: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 100, 112, 113]), remove all odd numbers, leaving only the even numbers.
+
+// Solution:
+
+var a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 100, 112, 113];
+
+function onlyEven(array){
+    if (Array.isArray(array)) {
+        var arr = [];
+        function checkEvens(array){
+            if (array.length > 0){
+                var curr = array.shift();
+                if (curr % 2 === 0) arr.push(curr);
+                checkEvens(array);
+            } else {
+                return arr;
+            }
+        };
+        checkEvens(array);
+        return arr;
+    }else{
+        return console.log(array + " is not an array!");
+    }
+};
+
+onlyEven(a);
+            
